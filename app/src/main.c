48b1ca8bc05204f5bba5f3d1f68e6bef55b3dab3
Daniel Pardo <dpardo25@proton.me>
2025-08-19 20:10:16 +0000

    Added DHT11 to overlay

--- app/src/main.c
+++ app/src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 
 // Settings
 static const int32_t sleep_time_ms = 1000;
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(my_led), gpios);
+static const struct gpio_dt_spec power_led = GPIO_DT_SPEC_GET(DT_ALIAS(power_led), gpios);
+static const struct device* temp_humidity_sensor = DEVICE_DT_GET(DT_ALIAS(temp_humidity_sensor));
 
 int main(void)
 {
@@ -12,12 +14,12 @@ int main(void)
 	int state = 0;
 
 	// Make sure that the GPIO was initialized
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&power_led)) {
 		return 0;
 	}
 
 	// Set the GPIO as output
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+	ret = gpio_pin_configure_dt(&power_led, GPIO_OUTPUT);
 	if (ret < 0) {
 		return 0;
 	}
@@ -30,7 +32,7 @@ int main(void)
 		printk("LED state: %d\r\n", state);
 		
 		// Set pin state
-		ret = gpio_pin_set_dt(&led, state);
+		ret = gpio_pin_set_dt(&power_led, state);
 		if (ret < 0) {
 			return 0;
 		}
