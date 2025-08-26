a63d59bac86f898353e8a81a61a00b3faa4c4c27
Daniel Pardo <dpardo25@proton.me>
2025-08-26 20:05:14 +0000

    Replaced printk with logger module

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
 // Settings
 static const int32_t sleep_time_ms = 1000;
@@ -15,25 +18,14 @@ int main(void)
 	struct sensor_value temp_value;
 	struct sensor_value humidity_value;
 
-	// Make sure that the GPIO was initialized
-	if (!gpio_is_ready_dt(&power_led))
-	{
-		printk("Error initializing LED.\n");
-		return 0;
-	}
+	// Ensure that the peripherals were initialized
+	__ASSERT(gpio_is_ready_dt(&power_led), "Error initializing LED.\n");
+	__ASSERT(device_is_ready(temp_humidity_sensor), "Error initializing temperature sensor.\n");
 
 	// Set the GPIO as output
 	if (gpio_pin_configure_dt(&power_led, GPIO_OUTPUT) < 0)
 	{
-		printk("Error configuring LED pin.\n");
-		return 0;
-	}
-
-	// Initialize the temperature and humidity sensor
-	if (!device_is_ready(temp_humidity_sensor))
-	{
-		printk("Error initializing temperature sensor.\n");
-		return 0;
+		LOG_ERR("Error configuring LED pin.\n");
 	}
 
 	while (1)
@@ -42,12 +34,12 @@ int main(void)
 		led_state = !led_state;
 		if (gpio_pin_set_dt(&power_led, led_state) < 0)
 		{
-			printk("Error setting GPIO pin\n");
+			LOG_ERR("Error setting GPIO pin\n");
 		}
 
 		// Read temperature and humidity
         get_temperature_and_humidity(temp_humidity_sensor, &temp_value, &humidity_value);
-        printk("Temperature: %d.%06d C, Humidity: %d.%06d %%\n",
+        LOG_INF("Temperature: %d.%06d C, Humidity: %d.%06d %%\n",
                temp_value.val1, temp_value.val2,
                humidity_value.val1, humidity_value.val2);
 
@@ -64,18 +56,18 @@ void get_temperature_and_humidity(struct device* pSensor, struct sensor_value* p
 	int result = sensor_sample_fetch(pSensor);
 	if (result < 0)
 	{
-		printk("Error fetching sensor data: %d\n", result);
+		LOG_ERR("Error fetching sensor data: %d\n", result);
 	}
 
 	// Read temperature and humidity channels
 	result = sensor_channel_get(pSensor, SENSOR_CHAN_AMBIENT_TEMP, pTempValue);
 	if (result < 0)
 	{
-		printk("Error reading temperature: %d\n", result);
+		LOG_ERR("Error reading temperature: %d\n", result);
 	}
     result = sensor_channel_get(pSensor, SENSOR_CHAN_HUMIDITY, pHumidityValue);
     if (result < 0)
     {
-        printk("Error reading humidity: %d\n", result);
+        LOG_ERR("Error reading humidity: %d\n", result);
     }
 }
